881d7aaec01e4977070d3dd3e74a5cc49236f284
alim7007420 <ali.7007.mohammadi@gmail.com>
2026-07-22 01:08:19 +0330

    Create enums.dart

---
+++ lib/db/enums.dart
@@ -0,0 +1,9 @@
+enum TaskPriority { low, medium, high, critical }
+
+enum TaskUnit { count, minutes, hours, pages, reps }
+
+enum HabitType { positive, negative }
+
+enum HabitFrequency { daily, weekly, custom }
+
+enum TransactionType { income, expense }
